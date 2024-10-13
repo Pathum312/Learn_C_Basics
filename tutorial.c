8d4ae8932ebcc0a0a57b80069891f75f8acac494
Pathum312 <pathum.2019557@iit.ac.lk>
2024-10-13 18:28:55 +0530

    Arithmetic Operators

--- tutorial.c
+++ tutorial.c
@@ -1,11 +1,39 @@
 #include <stdio.h>
 
 int main() {
-    // Constant - fixed value that cannot be altered by the program during its execution
+    // Arithmetic Operators
 
-    const float PI = 3.14159;
+    int x = 5;
+    int y = 2;
+    int z;
 
-    printf("%f", PI);
+    // + (Addition)
+    z = x + y;
+    printf("%d + %d = %d\n", x, y, z);
+
+    // - (Substraction)
+    z = x - y;
+    printf("%d - %d = %d\n", x, y, z);
+
+    // * (Multiplication)
+    z = x * y;
+    printf("%d * %d = %d\n", x, y, z);
+
+    // / (Division)
+    float ans = x / (float)y;
+    printf("%d / %d = %.1f\n", x, y, ans);
+
+    // % (Modules)
+    z = x % y;
+    printf("%d mod %d = %d\n", x, y, z);
+
+    // ++ Increment
+    x++;
+    printf("x++ = %d\n", x);
+
+    // -- Decrement
+    y--;
+    printf("y-- = %d\n", y);
 
     return 0;
 }
