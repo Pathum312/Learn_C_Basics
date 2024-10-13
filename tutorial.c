7ddecb87330ab81b72911b79e4990864ec0fa33b
Pathum312 <pathum.2019557@iit.ac.lk>
2024-10-13 16:38:24 +0530

    Format Specifiers

--- tutorial.c
+++ tutorial.c
@@ -2,40 +2,25 @@
 #include <stdio.h>
 
 int main() {
-    // Datatypes
+    // Format specifier % = defines and formats a type of data to be displayed
 
-    char a = 'C'; // Single Character %c
-    char b[] = "Pinkz"; // Array of characters %s
+    // %c - character
+    // %s - string (array of characters)
+    // %f - float
+    // %lf - double
+    // %d - integer
 
-    float c = 3.141592; // 4 bytes (32 bits of precision) 6 - 7 digits %f
-    double d = 3.141592653589793; // 8 bytes (64 bites of precision) 15 - 16 digits %lf
+    // %.1 - decimal precision
+    // %1 - minimum field width
+    // %- - left align
 
-    bool e = true; // 1 byte (true/1 or false/0) $d
+    float item1 = 5.75;
+    float item2 = 10.00;
+    float item3 = 100.99;
 
-    char f = 120; // 1 byte (-128 to +127) %d or %c
-    unsigned char g = 255; // 1 byte (0 tto +255) %d or %c
+    printf("Item 1: $%-8.2f\n", item1);
+    printf("Item 2: $%-8.2f\n", item2);
+    printf("Item 3: $%-8.2f\n", item3);
 
-    short h = 32767; // 2 bytes (-32768 to +32767) %d
-    unsigned short i = 32767; // 2 bytes (0 to +64535) %d
-
-    int j = 2147483647; // 4 bytes (-12147483648 to +2147483647) %d
-    unsigned int k = 4294967295; // 4 bytes (0 to +4294967295) %u
-
-    long long int l = 9223372036854775807; // 8 bytes (-9 quintillion to +9 quintillion) %lld
-    unsigned long long int m = 18446744073709551615U; // 8 bytes (0 to +18 quintillion) %llu
-
-    printf("%c\n", a); // char
-    printf("%s\n", b); // character array
-    printf("%f\n", c); // float
-    printf("%lf\n", d); // double
-    printf("%d\n", e); // bool
-    printf("%d %c\n", f, f); // char as a numeric value
-    printf("%d %c\n", g, g); // unsigned
-    printf("%d\n", h); // short
-    printf("%d\n", i); // unsigned short
-    printf("%d\n", j); // int
-    printf("%u\n", k); // unsigned int
-    printf("%lld\n", l); // long long int
-    printf("%llu\n", m); // unsigned long long int
     return 0;
 }
