fb2aa5798c5760b03216d42936bdb9cf282b7ec1
Pathum312 <pathum.2019557@iit.ac.lk>
2024-10-14 17:55:36 +0530

    Function Prototype

--- tutorial.c
+++ tutorial.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
-#include <math.h>
 
-int findMax(int x, int y)
-{
-    return (x > y) ? x : y;
-}
+int findMax(int, int); // Function prototype
 
 int main()
 {
-    // Ternary Operators - if/else statement shortcut
-    // (condition) ? value if true : value if false
+    /*
+    Function Prototype
+
+    What is it?
+        Function decalaration, without a body, before the main.
+        Ensures that calls to a function are made with the correct arguments.
+
+    Notes:
+        Many C compilers, do not check for parameter matching.
+        Missing arguments will results in weird behaviour.
+        A function prototype causes the complier to flag an error if the arguments are missing.
+
+    Advantages:
+        1. Easier to navigate a program with main at the top.
+        2. Helps with debugging.
+        3. Commonly used in header files.
+    */
 
     int max = findMax(3, 4);
     printf("%d", max);
 
     return 0;
+}
+
+int findMax(int x, int y)
+{
+    return (x > y) ? x : y;
 }
