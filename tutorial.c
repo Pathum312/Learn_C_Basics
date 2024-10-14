778fe5f598b6808185d425c9cfb21ffb450d6645
Pathum312 <pathum.2019557@iit.ac.lk>
2024-10-14 14:36:01 +0530

    IF Statements

--- tutorial.c
+++ tutorial.c
@@ -2,43 +2,22 @@
 #include <math.h>
 
 int main() {
-    // Math Functions
+    // IF Statments
 
-    // Square root
-    double A = sqrt(9);
-    printf("%lf\n", A);
+    int age;
 
-    // Power of
-    double B = pow(2, 4);
-    printf("%lf\n", B);
+    printf("\nEnter your age: ");
+    scanf("%d", &age);
 
-    // Round off
-    int C = round(3.14);
-    printf("%d\n", C);
-
-    // Round up
-    int D = ceil(3.14);
-    printf("%d\n", D);
-
-    // Round down
-    int E = floor(3.14);
-    printf("%d\n", E);
-
-    // Log
-    double G = log(3);
-    printf("%lf\n", G);
-
-    // SIN
-    double H = sin(45);
-    printf("%lf\n", H);
-
-    // COS
-    double I = cos(45);
-    printf("%lf\n", I);
-
-    // TAN
-    double J = tan(45);
-    printf("%lf\n", J);
+    if (age > 18) {
+        printf("\nYou have just signed up!");
+    }
+    else if (age < 0) {
+        printf("\nYou cannot sign up, you are not born yet!");
+    }
+    else {
+        printf("\nYou cannot signed up for %d more years!", 18 - age);
+    }
 
     return 0;
 }
